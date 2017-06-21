fc37d96fd08401b1fa495352d8e8eb37f717ede2
Stefan Pesic <sp.pesic@live.nl>
2017-06-21 08:28:59 +0200

    added main factory

--- Jaar 1 Project 4/Jaar 1 Project 4/Factory.cs
+++ Jaar 1 Project 4/Jaar 1 Project 4/Factory.cs
@@ -7,27 +7,16 @@ using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
+
 //The factory design pattern
 //TODO find out where the button must be loaded
 
 namespace Jaar_1_Project_4 {
-    class Factory {
-    }
-    //The ButtonFactory creates a button
-    public abstract class ButtonFactory {
-        public enum HorizontalAllignmentEnum { left, right, center, stretch }
-        public enum VerticalAlignmentEnum  {top,center, stretch, bottom }
-        public static Button CreateButton(string text, HorizontalAllignmentEnum horizontalAllignment, VerticalAlignmentEnum verticalAllignment, double rectLeftTickness, double rectUpperTickness, double rectRighTickness, double rectLowerTickness, int height, int width) {
-            Button button = new Button();
-            button.Content = text;
-            button.HorizontalAlignment = convertHorizontalAllignment(horizontalAllignment);
-            button.VerticalAlignment = converVerticalAllignment(verticalAllignment);
-            button.Margin = new Thickness(rectLeftTickness, rectUpperTickness, rectRighTickness, rectLowerTickness);
-            button.Height = height;
-            button.Width = width;
-  
-            return button;
-        }
+    class Factory { }
+    //The main factory
+    public abstract class ElementCreatorMainfactory {
+        public enum HorizontalAllignmentEnum { left, right, center, stretch } //Enum is used because it's safer than using strings
+        public enum VerticalAlignmentEnum { top, center, stretch, bottom }
         private static HorizontalAlignment convertHorizontalAllignment(HorizontalAllignmentEnum position) {
             switch (position) {
                 case (HorizontalAllignmentEnum.left):
@@ -55,6 +44,26 @@ namespace Jaar_1_Project_4 {
                 default:
                     return VerticalAlignment.Center;
             }
+
         }
+        //Button factory creates buttons
+        public abstract class ButtonFactory : ElementCreatorMainfactory {
+            public static Button CreateButton(string text, HorizontalAllignmentEnum horizontalAllignment, VerticalAlignmentEnum verticalAllignment, double rectLeftTickness, double rectUpperTickness, double rectRighTickness, double rectLowerTickness, int height, int width) {
+                Button button = new Button();
+                button.Content = text; //The values of the buttons are set with the setters of the button class
+                button.HorizontalAlignment = convertHorizontalAllignment(horizontalAllignment);
+                button.VerticalAlignment = converVerticalAllignment(verticalAllignment);
+                button.Margin = new Thickness(rectLeftTickness, rectUpperTickness, rectRighTickness, rectLowerTickness);
+                button.Height = height;
+                button.Width = width;
+                return button;
+            }
+        }
+        //Label factory creates labels
+        public abstract class LabelFactory : ElementCreatorMainfactory {
+            public static TextBlock createLabel() //TODO create label method
+
+        }
+
     }
 }
