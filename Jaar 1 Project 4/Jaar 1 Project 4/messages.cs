d9366ed9aa6d5416bb6680e970cb56da8c671afe
Ino van den Berg <0928787@hr.nl>
2017-06-21 12:11:17 +0200

    started the message factory

---
+++ Jaar 1 Project 4/Jaar 1 Project 4/messages.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jaar_1_Project_4
+{
+    interface Message { }
+    abstract class MessageFactory
+    {
+        public enum MessageType { question, answer };
+        public static Message Create(MessageType type)
+        {
+            switch (type)
+            {
+                case MessageType.question:
+                    {
+                        return new Question();
+                    }
+                case MessageType.answer:
+                    {
+                        return new Answer();
+                    }
+                default:
+                    {
+                        return null;
+                    }
+            }
+        }
+    }
+    public class Question : Message
+    {
+
+    }
+    public class Answer : Message
+    {
+
+    }
+}
