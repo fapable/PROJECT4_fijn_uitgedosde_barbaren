d5c72ca30912b4c4a19db08f23e03c1a2f2085d4
Ino van den Berg <0928787@hr.nl>
2017-06-22 13:38:44 +0200

    More work on the message factory

--- Jaar 1 Project 4/Jaar 1 Project 4/messages.cs
+++ Jaar 1 Project 4/Jaar 1 Project 4/messages.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 
-namespace Jaar_1_Project_4
+namespace Jaar_1_Project_4_Messages
 {
     interface Message { }
     abstract class MessageFactory
     {
-        public enum MessageType { question, answer };
+        public enum MessageType { question, answer, notification };
         public static Message Create(MessageType type)
         {
             switch (type)
@@ -21,6 +28,10 @@ namespace Jaar_1_Project_4
                 case MessageType.answer:
                     {
                         return new Answer();
+                    }
+                case MessageType.notification:
+                    {
+                        return new Notification();
                     }
                 default:
                     {
@@ -36,5 +47,36 @@ namespace Jaar_1_Project_4
     public class Answer : Message
     {
 
+    }
+    public class Notification : Message
+    {
+
+    }
+    public class EasyLabel
+    {
+        int width;
+        int height;
+        int x;
+        int y;
+        string text;
+        dynamic current_page;
+        public EasyLabel(int width, int height, int x, int y, string text, Page current_page)
+        {
+            this.width = width;
+            this.height = height;
+            this.x = x;
+            this.y = y;
+            this.text = text;
+            this.current_page = current_page;
+        }
+        public void draw()
+        {
+            var current_message = new TextBlock();
+            current_message.Text = this.text;
+            current_message.Width = this.width;
+            current_message.Height = this.height;
+            current_message.RenderTransform = new TranslateTransform { X = this.x, Y = this.y };
+            this.current_page.Children.Add(current_message);
+        }
     }
 }
