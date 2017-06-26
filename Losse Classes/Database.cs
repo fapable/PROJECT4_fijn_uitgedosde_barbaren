dba032322486868aaa4d899d236bcc806848dd39
ilhamelb <ilhaam_@live.nl>
2017-06-26 12:20:27 +0200

    Added Database

---
+++ Losse Classes/Database.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Database
+{
+    private static DataSet dataSet = new DataSet();
+    private static DataTable dataTable = new DataTable();
+
+	public Database()
+	{
+        string Connection = String.Format("Server = {0}; Port = {1};" + "User ID = {2}; Password = {3}; Database = {4};", "145.24.222.148", "5432", "postgres", "1kb3n33n@dm1n", "project4");
+        NpgsqlConnection connectie = new NpgsqlConnection(Connection);
+	}
+
+    public DataTable getInfo(string sql)
+    {
+        connectie.Open();
+        NpgsqlDataAdapter Data = new NpgsqlDataAdapter(sql, connectie);
+
+        dataSet.Reset();
+        Data.Fill(dataSet);
+        dataTable = dataSet.Tables[0];
+
+        connectie.Close();
+        return DataTable;
+    }
+}
