a807387ffd9515d11a2b801263ad3f3713e7540c
ilhamelb <ilhaam_@live.nl>
2017-06-27 11:36:04 +0200

    database function work

--- Losse Classes/Database.cs
+++ Losse Classes/Database.cs
@@ -1,4 +1,5 @@
 ﻿// we use these libraries
+// TODO import system.data for use of dataset en datatable
 using System;
 using Npgsql;
 
@@ -9,17 +10,21 @@ public class Database
     private static DataSet dataSet = new DataSet();
     private static DataTable dataTable = new DataTable();
 
+    // TODO voer checks uit op de strings doormiddel van if, else if en else om ervoor te zorgen dat de correcte SQL query is opgegeven
+    // Bijvoorbeeld: if(sql[:6] == "SELECT") { next check }
+
 
     // we create the Database variables
-	public Database()
-	{
+    public Database()
+    {
         string Connection = String.Format("Server = {0}; Port = {1};" + "User ID = {2}; Password = {3}; Database = {4};", "145.24.222.148", "5432", "postgres", "1kb3n33n@dm1n", "project4");
         NpgsqlConnection connectie = new NpgsqlConnection(Connection);
-	}
+    }
 
     // we get Info from the Database 
     public DataTable getInfo(string sql)
     {
+        // TODO Insert a try to catch any potential errors
         // we open the connection of our sql database
         connectie.Open();
         // we assign a new variable to store our Info
@@ -32,12 +37,14 @@ public class Database
 
         // the connection closes
         connectie.Close();
+
         // we return the DataTable
-        return DataTable;
+        return dataTable;
     }
     // we add Info to the Database
-    public void addInfo(string sql)
-    {   
+    public void editInfo(string sql)
+    {
+        // TODO Insert a try to catch any potential errors
         // opens connection
         connectie.Open();
 
