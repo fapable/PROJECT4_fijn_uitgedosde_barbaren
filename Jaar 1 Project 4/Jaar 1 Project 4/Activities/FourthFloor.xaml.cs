72ab689020b7d2f182918e6636d57c1ca676d4ec
Ino van den Berg <0928787@hr.nl>
2017-06-29 10:56:57 +0200

    Revert "the buttons (classroom) are recognized"
    This reverts commit e6c23484fb1af4a5669c1ed39f5a84c60cb1a7f6.

--- Jaar 1 Project 4/Jaar 1 Project 4/Activities/FourthFloor.xaml.cs
+++ Jaar 1 Project 4/Jaar 1 Project 4/Activities/FourthFloor.xaml.cs
@@ -13,7 +13,6 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Windows.Graphics.Display; //For the flip
-using System.Diagnostics;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -29,31 +28,6 @@ namespace Jaar_1_Project_4 {
 
         private void fourthFlourBackButton_Click(object sender, RoutedEventArgs e) {
             this.Frame.Navigate(typeof(Activities));
-        }
-        //When a button(classroom) gets clicked, this method gets activated
-        private void SendQuery(object sender, RoutedEventArgs e) {
-           Button button = (Button) sender; //sender gets cast to button
-            string emptyButtonName = ""; //buttonname will be saved in this
-            foreach (var character in button.Name.ToString()) { //buttonname gets looped
-                if (character.ToString() == "_") {
-                    emptyButtonName += ".";
-                }
-                else {
-                    emptyButtonName += character.ToString();
-                }
-            }
-            this.CreatePopUp();
-            Debug.WriteLine("Button, what is your name? My name is: " + emptyButtonName);
-        }
-        //Creates the pop up that will appear on the screen
-        //NOT DONE
-        public void CreatePopUp() {
-            Border popupBorder = new Border();
-            
-            Thickness thickness = new Thickness(123, 45, 0, 10);
-            popupBorder.Margin = thickness;
-
-            fourth_floor.Children.Add(popupBorder);
         }
     }
 }
