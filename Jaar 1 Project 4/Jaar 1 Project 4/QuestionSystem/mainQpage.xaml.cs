037bc29ae5e0bd5c63157a0ef8b07a43c79edbb9
Ino van den Berg <0928787@hr.nl>
2017-06-29 12:01:52 +0200

    added more menu's for the questions

--- Jaar 1 Project 4/Jaar 1 Project 4/QuestionSystem/mainQpage.xaml.cs
+++ Jaar 1 Project 4/Jaar 1 Project 4/QuestionSystem/mainQpage.xaml.cs
@@ -20,9 +20,9 @@ namespace Jaar_1_Project_4.QuestionSystem
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class QandAPage : Page
+    public sealed partial class mainQpage : Page
     {
-        public QandAPage()
+        public mainQpage()
         {
             this.InitializeComponent();
         }
