f613e7e333dbc989b6b55bdf8f6233e9f2da3494
Ino van den Berg <0928787@hr.nl>
2017-06-29 15:00:00 +0200

    1 message is displayed properly

--- Jaar 1 Project 4/Jaar 1 Project 4/QuestionSystem/AskAQuestions.xaml.cs
+++ Jaar 1 Project 4/Jaar 1 Project 4/QuestionSystem/AskAQuestions.xaml.cs
@@ -19,22 +19,26 @@ namespace Jaar_1_Project_4 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Questions : Page {
-        public Questions() {
+    public sealed partial class Questions : Page
+    {
+        public Questions()
+        {
             this.InitializeComponent();
         }
 
-        private void mainTitle_SelectionChanged(object sender, RoutedEventArgs e) {
+        private void mainTitle_SelectionChanged(object sender, RoutedEventArgs e)
+        {
 
         }
 
-        private void back_Click(object sender, RoutedEventArgs e) {
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
             this.Frame.Navigate(typeof(MainMenu));
         }
 
         private void send_message(object sender, RoutedEventArgs e)
         {
-            Jaar_1_Project_4_Messages.Message question = Jaar_1_Project_4_Messages.MessageFactory.Create(Jaar_1_Project_4_Messages.MessageType.question, new Jaar_1_Project_4_Messages.EasyLabel(-(getWidth() / 2) + 5,50, getWidth() - 20 ,"vandaaggaanwijeenheellulverhaalschrijvenzonderspatiets om iets uit te testen en opeen ga ik spatioes gebruiken wtf haha oke ben ik al ver genoeg met dit kut verhaal vast wel hihi"), askquestions);
+            Jaar_1_Project_4_Messages.Message question = Jaar_1_Project_4_Messages.MessageFactory.Create(Jaar_1_Project_4_Messages.MessageType.question, new Jaar_1_Project_4_Messages.EasyLabel(-(getWidth() / 2) + 120, -250, getWidth() - 300, 180, "vandaaggaanwijeenheellulverhaalschrijvenzonderspatiets om iets uit te testen en opeen ga ik spatioes gebruiken wtf haha oke ben ik al ver genoeg met dit kut verhaal vast wel hihi"), askquestions);
             question.Draw();
         }
 
@@ -50,6 +54,16 @@ namespace Jaar_1_Project_4 {
             {
                 double Width = pnlClient.ActualWidth;
                 return (int)Width;
+            }
+            return 0;
+        }
+        private int getHeight()
+        {
+            FrameworkElement pnlClient = this.Content as FrameworkElement;
+            if (pnlClient != null)
+            {
+                double Height = pnlClient.ActualHeight;
+                return (int)Height;
             }
             return 0;
         }
