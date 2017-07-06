706bedc3d1bdee3a5992a781b9f598b0885ccced
ilhamelb <ilhaam_@live.nl>
2017-07-06 15:01:40 +0200

    PERCENTAGESS

---
+++ Jaar 1 Project 4/Jaar 1 Project 4/QuestionSystem/TextToURL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jaar_1_Project_4.QuestionSystem
+{
+    class TextToURL
+    {
+        public static string text_to_string(string text)
+        {
+            string result = "";
+
+            result = result.Replace("%", "%25");
+            result = result.Replace(" ", "-");
+            result = result.Replace("#", "%23");
+            result = result.Replace("!", "%21");
+            result = result.Replace("@", "%40");
+            result = result.Replace("$", "%24");
+            result = result.Replace("*", "%2A");
+            result = result.Replace("+", "%2B");
+            result = result.Replace("?", "%3F");
+
+            return result;
+        }
+
+
+
+    }
+}
