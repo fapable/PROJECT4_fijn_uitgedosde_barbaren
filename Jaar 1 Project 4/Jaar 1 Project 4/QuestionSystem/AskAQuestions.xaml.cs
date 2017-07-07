8de4dbc1e2a2e4007ba2eefd4b8ab1b2002eeb98
Ino van den Berg <0928787@hr.nl>
2017-07-07 10:03:06 +0200

    fixed the crash

--- Jaar 1 Project 4/Jaar 1 Project 4/QuestionSystem/AskAQuestions.xaml.cs
+++ Jaar 1 Project 4/Jaar 1 Project 4/QuestionSystem/AskAQuestions.xaml.cs
@@ -31,22 +31,29 @@ namespace Jaar_1_Project_4 {
             var syncClient = new HttpClient(); //allow api connection
             //variables that need to be uploaded to the database
             string email = E_mail.Text;
-            var eduselect = Opleiding.Items[Opleiding.SelectedIndex] as ComboBoxItem;
-            string education = eduselect.Content.ToString();
-            string name = Naam.Text;
-            string question = Vraag.Text;
-            string qid = "http://www.wschaijk.nl/api/api.php/SELECT-question_id-FROM-questions-ORDER-BY-question_id-DESC-LIMIT-1;"; //query for current highest question id
-            var qidCall = syncClient.GetStringAsync(qid);
-            var qidResult = qidCall.Result; //result for the query
-            var gimmeResult = new PrepareForScreenQueryHandler();
-            var tempid = gimmeResult.ResultOnly(qidResult); //remove unnecessary string elements from result
-            int id = Int32.Parse(tempid); //go from string result to int result
-            id = id + 1; //increment current highest id by one because youre adding a new question
-            string qupload = TextToURL.text_to_string(string.Format("http://www.wschaijk.nl/api/api.php/INSERT-INTO-questions-VALUES({0},-\'{1}\',-\'{2}\',-\'{3}\',-\'{4}\');", id, education, email, name, question)); //upload the question
-            var uploadstuff = syncClient.GetAsync(qupload);
-            string tempanswer = TextToURL.text_to_string(string.Format("http://www.wschaijk.nl/api/api.php/INSERT-INTO-answer-VALUES({0},-\'{1}\',-\'place\',-\'This-question-is-not-yet-answered.\');", id, education)); //upload a dummy answer
-            var uploadta = syncClient.GetAsync(tempanswer);
-            this.Frame.Navigate(typeof(Jaar_1_Project_4.QuestionSystem.mainQpage)); //send user back to main question page
+            try
+            {
+                var eduselect = Opleiding.Items[Opleiding.SelectedIndex] as ComboBoxItem;
+                string education = eduselect.Content.ToString();
+                string name = Naam.Text;
+                string question = Vraag.Text;
+                string qid = "http://www.wschaijk.nl/api/api.php/SELECT-question_id-FROM-questions-ORDER-BY-question_id-DESC-LIMIT-1;"; //query for current highest question id
+                var qidCall = syncClient.GetStringAsync(qid);
+                var qidResult = qidCall.Result; //result for the query
+                var gimmeResult = new PrepareForScreenQueryHandler();
+                var tempid = gimmeResult.ResultOnly(qidResult); //remove unnecessary string elements from result
+                int id = Int32.Parse(tempid); //go from string result to int result
+                id = id + 1; //increment current highest id by one because youre adding a new question
+                string qupload = TextToURL.text_to_string(string.Format("http://www.wschaijk.nl/api/api.php/INSERT-INTO-questions-VALUES({0},-\'{1}\',-\'{2}\',-\'{3}\',-\'{4}\');", id, education, email, name, question)); //upload the question
+                var uploadstuff = syncClient.GetAsync(qupload);
+                string tempanswer = TextToURL.text_to_string(string.Format("http://www.wschaijk.nl/api/api.php/INSERT-INTO-answer-VALUES({0},-\'{1}\',-\'place\',-\'This-question-is-not-yet-answered.\');", id, education)); //upload a dummy answer
+                var uploadta = syncClient.GetAsync(tempanswer);
+                this.Frame.Navigate(typeof(Jaar_1_Project_4.QuestionSystem.mainQpage)); //send user back to main question page
+            }
+            catch (System.ArgumentOutOfRangeException)
+            {
+                this.Frame.Navigate(typeof(Questions));
+            }
         }
     }
 }
